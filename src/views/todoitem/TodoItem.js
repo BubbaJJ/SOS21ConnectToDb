9d5adc898c4d0e97dcde32befc6ee3f4bf57d74b
Jim Kaya <Jim.kaya@gmail.com>
2022-05-27 15:15:23 +0200

    WIP

--- src/views/todoitem/TodoItem.js
+++ src/views/todoitem/TodoItem.js
@@ -5,15 +5,26 @@ import Moment from "moment";
 const TodoItem = () => {
   const [value, setValue] = useState("");
   const [todo, setTodo] = useState({});
+  const [updatedTodo, setUpdatedTodo] = useState({});
   const [todoFound, setTodoFound] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const handleSubmit = async () => {
     let data = await GetTodoById(value);
+    setEditMode(false);
     if (data.status !== 404) {
       setTodo(data);
+      setUpdatedTodo(data);
       setTodoFound(true);
     } else {
       setTodoFound(false);
     }
+  };
+
+  const handleInputChange = (event) => {
+    setUpdatedTodo((updatedTodo) => ({
+      ...updatedTodo,
+      [event.target.name]: event.target.value,
+    }));
   };
 
   if (todoFound) {
@@ -34,6 +45,7 @@ const TodoItem = () => {
               <th>Date added</th>
               <th>Due date</th>
               <th>{todo.dateFinished ? "Date finished" : null}</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -48,9 +60,96 @@ const TodoItem = () => {
                   ? Moment(todo.dateFinished).format("MMM Do YYYY")
                   : null}
               </td>
+              <td>
+                <button onClick={() => setEditMode(true)}>Edit</button>
+              </td>
             </tr>
           </tbody>
         </table>
+        {editMode ? (
+          <div>
+            <h1>Edit task</h1>
+            <form>
+              <table>
+                <thead>
+                  <tr>
+                    <th colSpan="2">{todo.name}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Task name:</td>
+                    <td>
+                      <input
+                        type="text"
+                        name="name"
+                        defaultValue={todo.name}
+                        onChange={(event) => handleInputChange(event)}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Completed:</td>
+                    <td>
+                      <input
+                        type="checkbox"
+                        name="completed"
+                        value={updatedTodo.completed}
+                        onChange={(event) => handleInputChange(event)}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Date added:</td>
+                    <td>
+                      <input
+                        type="date"
+                        name="dateAdded"
+                        value={Moment(todo.dateAdded).format("yyyy-MM-DD")}
+                        disabled
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Date due:</td>
+                    <td>
+                      <input
+                        type="date"
+                        name="dueDate"
+                        defaultValue={Moment(todo.dueDate).format("yyyy-MM-DD")}
+                        onChange={(event) => handleInputChange(event)}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Date finished:</td>
+                    <td>
+                      <input
+                        type="date"
+                        name="dateFinished"
+                        value={
+                          updatedTodo.dateFinished
+                            ? Moment(updatedTodo.dateFinished).format(
+                                "yyyy-MM-DD"
+                              )
+                            : ""
+                        }
+                        onChange={(event) => handleInputChange(event)}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan="2">
+                      <button type="submit">Save</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </form>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     );
   } else {
